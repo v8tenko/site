64332eaa880fe7178738dd41ac28e1db5f3e3edd
v8tenko <glebik8@gmail.com>
2021-02-22 01:39:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "react-bootstrap";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="center">
+        <div className="buttons-top">
+          <button id="load" className="btn btn-primary ml-3 mt-3"> Загрузить файл </button>
+          <button id="download" className="btn btn-success ml-3 mt-3"> Скачать полученное расписание</button>
+        </div>
+        <div className="content">
+
+        </div>
+      </div>
     </div>
   );
 }
